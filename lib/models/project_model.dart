8367f97c27d35b20aacb0ec8e61e56fecfa5dc6e
Bhavesh <bhaveshkewalramani08@gmail.com>
2026-03-27 21:05:49 +0530

    Add ProjectModel class definition

---
+++ lib/models/project_model.dart
@@ -0,0 +1,7 @@
+class ProjectModel {
+  final String title;
+  final String description;
+  final String imageUrl;
+
+  ProjectModel({required this.title, required this.description, required this.imageUrl});
+}
